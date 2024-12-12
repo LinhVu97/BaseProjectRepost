cdaddaef2057979fc749072f442096c29acd20ff
LinhVu <linhvn2303@gmail.com>
2024-12-12 14:30:01 +0700

    Update Base Project

---
+++ BaseProjectRepost/Models/ExampleModel.swift
@@ -0,0 +1,12 @@
+//
+//  ExampleModel.swift
+//  BaseProjectRepost
+//
+//  Created by Linh Vu on 12/12/24.
+//
+
+import Foundation
+
+struct ExampleModel {
+    var name: String
+}
---
+++ BaseProjectRepost/Views/CustomViews/CustomTabbar.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabbar.swift
+//  BaseProjectRepost
+//
+//  Created by Linh Vu on 12/12/24.
+//
+
+import UIKit
+
+class CustomTabbar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
